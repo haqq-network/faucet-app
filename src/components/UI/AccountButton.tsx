7a7e9eff884acc2054909290065a3de5d652afb9
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-07-28 04:52:57 +0300

    feat: AccountButton, Button, OutlineButton

---
+++ src/components/UI/AccountButton.tsx
@@ -0,0 +1,65 @@
+import clsx from 'clsx';
+import React, { ReactElement } from 'react';
+import rectangle from '../../assets/rectangle.svg';
+
+interface WalletButtonProps {
+  account: {
+    account: string;
+    balance: string;
+  };
+  onAddressClick?: () => void;
+  onBalanceClick?: () => void;
+}
+
+export function AccountButton(props: WalletButtonProps): ReactElement {
+  const balance =
+    Number(props.account.balance) > 0
+      ? parseFloat(props.account.balance).toFixed(2)
+      : '0';
+  const hiddenBalance = `${props.account.account.slice(
+    0,
+    2,
+  )}...${props.account.account.slice(40, 42)}`;
+
+  const balanceClassNames = clsx(
+    'flex items-center justify-between bg-[#04D484] text-white text-base font-semibold not-italic gap-2.5 rounded-md leading-6 py-[2px] max-w-[212px] min-h-[40px] ',
+  );
+
+  const addressClassNames = clsx(
+    'flex bg-white text-black text-base leading-6 text-[14px] max-w-[83px] min-h-[36px] font-normal not-italic rounded-md justify-between items-center px-1 relative gap-[8px] right-[2px]',
+  );
+
+  const connectClassNames = clsx(
+    'bg-[#04D484] text-white text-base font-semibold not-italic gap-2.5 rounded-lg leading-6 py-2 px-4 w-40 h-10',
+  );
+
+  return (
+    <div>
+      {props?.account.account ? (
+        <div className={balanceClassNames}>
+          <span
+            className="ml-[10px]"
+            onClick={props.onBalanceClick}
+            style={
+              balance.length > 10
+                ? { fontSize: '11.7px' }
+                : { fontSize: '14px' }
+            }
+          >
+            {balance} ISLM
+          </span>
+          <div className={addressClassNames}>
+            <span onClick={props.onAddressClick}>{hiddenBalance}</span>
+            <img
+              src={rectangle}
+              alt="tokenLogo"
+              className="w-[16px] h-[16px]"
+            />
+          </div>
+        </div>
+      ) : (
+        <button className={connectClassNames}>Connect Wallet</button>
+      )}
+    </div>
+  );
+}
--- src/components/UI/OutlineButton.tsx
+++ src/components/UI/OutlineButton.tsx
@@ -1,9 +1,39 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, ReactNode } from 'react';
+import clsx from 'clsx';
 
-export function OutlineButton(): ReactElement {
+interface ButtonProps {
+  children: ReactNode;
+  className?: string;
+  disabled?: boolean;
+  block?: boolean;
+  fill?: boolean;
+  type?: 'submit' | 'reset' | 'button';
+  onClick?: () => void;
+}
+
+export function OutlineButton({
+  children,
+  className,
+  disabled = false,
+  block = false,
+  fill = false,
+  type,
+  onClick,
+}: ButtonProps): ReactElement {
+  const classNames = clsx(
+    'bg-transparent text-[#04D484] border-2 border-[#04D484] border-solid text-base font-semibold not-italic rounded-md leading-6 w-[160px] h-[40px]',
+    fill ? 'w-full' : 'inline-block',
+    { 'opacity-75 cursor-not-allowed': disabled },
+    className,
+  );
   return (
-    <button className="bg-transparent text-[#04D484] border-2 border-emerald-400 border-solid text-base font-semibold not-italic rounded-md leading-6  w-[160px] h-[40px]">
-      Send
+    <button
+      className={classNames}
+      onClick={onClick}
+      disabled={disabled}
+      type={type}
+    >
+      {children}
     </button>
   );
 }
