1d7f816e62392db3d517f326c7a4ec102c9e61c3
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-07-29 23:42:46 +0300

    fix: HeaderLink clsx remove

--- src/components/UI/HeaderLink.tsx
+++ src/components/UI/HeaderLink.tsx
@@ -1,5 +1,4 @@
-import clsx from 'clsx';
-import React, { ReactNode } from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 import { NavLink } from 'react-router-dom';
 
 interface HeaderLinkProps {
@@ -7,15 +6,11 @@ interface HeaderLinkProps {
   children: ReactNode;
 }
 
-export function HeaderLink({ children, to }: HeaderLinkProps) {
+export function HeaderLink({ children, to }: HeaderLinkProps): ReactElement {
   return (
     <NavLink
       to={to}
-      className={() => {
-        return clsx(
-          'text-black text-base text-center font-normal hover:text-[#04d484] underline-offset-0 hover:underline active:text-[#04d484]',
-        );
-      }}
+      className="text-black text-base text-center font-normal hover:text-[#04d484] underline-offset-0 hover:underline active:text-[#04d484]"
     >
       {children}
     </NavLink>
