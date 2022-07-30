b3413b63e5913090349c1a233a5b507b057f5251
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-07-30 04:33:38 +0300

    feat: blank Card

---
+++ src/components/UI/Card.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+export const Card: FC = () => {
+  return (
+    <>
+      {/* TODO div height HUG === h-auto? */}
+      <div className="w-[640px] h-[676px] bg-white text-black rounded-[32px]" />
+    </>
+  );
+};
